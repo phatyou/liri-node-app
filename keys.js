2ea70cf502d1b4cef77533c88556cf51cd7d220a
phatyou <jen.phatmandu7@gmail.com>
2017-08-26 15:29:18 -0600

    Changes for liri and random

---
+++ keys.js
@@ -0,0 +1,11 @@
+console.log('this is loaded');
+
+// Exports is essentially an object that we can add data or variables to
+// We can access them from other files using the 'require' keyword.
+exports.twitterKeys = {
+  consumer_key: 'W9WDxQ2DndjyG9oNrLkyNTLhM',
+  consumer_secret: 'QP0kNdll3ovFmVFNWwXszFGK0qihIQCoXtuWQEz0wQuTHM2hSo',
+  access_token_key: '898357046604578816-FG7Tzpaxhwa9OxwYzh9kqTbbFe5kd60',
+  access_token_secret: '2UJxr8IO93nblhL1VIm6TAw2wHd5AclvfHYbXbJQ0YH82',
+};
+
